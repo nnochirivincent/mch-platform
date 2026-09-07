b18f18bae8c3ce75b8081b5ad75694f3ea13b13f
Vincent <nnochirivincent115@gmail.com>
2026-09-07 11:40:36 +0100

    new commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
 import AdminDashboard from './components/AdminDashboard';
+import About from './components/About';
 
 export default function App() {
   return (
@@ -15,6 +16,7 @@ export default function App() {
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/admin" element={<AdminDashboard />} />
+            <Route path="/about" element={<About  />} />
           </Routes>
         </main>
 
